9965094e0277ff24206be5a17467ebb0f95ba18e
marianettimatias <marianettimatiass@gmail.com>
2022-10-11 17:33:51 -0300

    contact ok falta estilo al alert

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,43 +1,62 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { BsLinkedin } from 'react-icons/bs'
 import { BsGithub } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
+
 
 
 const Contact = () => {
-  return (
-    <section id='contact'>
-      <h2 className='contact_h2'>Contactame!</h2>
 
-      <div className="container contact_container">
-        <div className="contact_options">
+  const form = useRef();
 
-          <article className='contact_option'>
-            <BsLinkedin className='contact_option-icon'/>
-            <h4>Linkedin</h4>
-            <a href="https://www.linkedin.com/in/matias-marianetti/">Conoceme mejor</a>
-          </article>
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-          <article className='contact_option'>
-            <BsGithub className='contact_option-icon'/>
-            <h4>Github</h4>
-            <a href="https://github.com/marianettimatias">Mirá mis proyectos!</a>
-          </article>
+    emailjs.sendForm('service_0hfoh5c', 'template_y2lvg78', form.current, '2usp547788T0dk_II')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+      alert('Mensaje enviado')
+      e.target.reset();
+    }
+
+
+    return (
+      <section id='contact'>
+        <h2 className='contact_h2'>Contactame!</h2>
+
+        <div className="container contact_container">
+          <div className="contact_options">
+
+            <article className='contact_option'>
+              <BsLinkedin className='contact_option-icon' />
+              <h4>Linkedin</h4>
+              <a href="https://www.linkedin.com/in/matias-marianetti/">Conoceme mejor</a>
+            </article>
+
+            <article className='contact_option'>
+              <BsGithub className='contact_option-icon' />
+              <h4>Github</h4>
+              <a href="https://github.com/marianettimatias">Mirá mis proyectos!</a>
+            </article>
+
+          </div>
+
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name='name' placeholder='Nombre' required />
+            <input type="emailS" name='email' placeholder='Tu email' />
+            <textarea name="message" id="7" cols="30" rows="10" placeholder='Mensaje...' required></textarea>
+            <button type='submit' className='btn btn-primary'>Enviar mensaje</button>
+
+          </form>
 
         </div>
 
-        <form action="">
-          <input type="text" name='name' placeholder='Nombre' required />
-          <input type="emailS" name='email' placeholder='Tu email' />
-          <textarea name="message" id="7" cols="30" rows="10" placeholder='Mensaje...' required></textarea>
-          <button type='submit' className='btn btn-primary'>Enviar mensaje</button>
-          
-        </form>
+      </section>
+    )
+  }
 
-      </div>
-
-    </section>
-  )
-}
-
-export default Contact
+  export default Contact
