5483e1f0c2b2d861c36357ece92cb60ee02a0a44
marianettimatias <marianettimatiass@gmail.com>
2022-10-14 16:59:06 -0300

    query comentada

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
 
           Hola, mi nombre es Matías soy Ingeniero Industrial y recientemente graduado como Desarrollador Web Full Stack. Soy una persona proactiva, analítica y apasionado por la tecnología. En mis trabajos anteriores participé en proyectos de desarrollo de productos lo cual me brindo mucha experiencia en la gestión del tiempo y el trabajo en equipo.
           <br />
-          Soy nuevo en el mundo IT, pero en HENRY, tuve la suerte de trabajar en una SPA (single page application) usando muchas tecnologías como #JavaScript #React #Redux #CSS #Git #Node #Sequelize #PostgreSQL.
+          Soy nuevo en el mundo IT, pero en HENRY, tuve la suerte de trabajar varios proyectos usando muchas tecnologías como #JavaScript #React #Redux #CSS #Git #Node #Sequelize #PostgreSQL.
         </p>
 
         <HeaderSocials />
